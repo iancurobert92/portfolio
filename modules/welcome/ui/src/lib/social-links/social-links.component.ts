3a49cd58464dcfea90cd2b7681a2fb1657fc768b
iancu_robert <robert.iancu@zupria.com>
2024-09-14 15:28:57 +0300

    Implement social-links component

--- modules/welcome/ui/src/lib/social-links/social-links.component.ts
+++ modules/welcome/ui/src/lib/social-links/social-links.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDefinition } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'lib-social-links',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './social-links.component.html',
   styleUrl: './social-links.component.scss',
 })
-export class SocialLinksComponent {}
+export class SocialLinksComponent {
+  @Input({ required: true }) icons: IconDefinition[] = [];
+}
