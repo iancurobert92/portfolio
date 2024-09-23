94040c6d58c622cb63b232c6a790e1e0d8f3392e
iancu_robert <iancurobert92@gmail.com>
2024-09-23 16:18:17 +0300

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,42 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signOut,
+  user,
+  UserCredential,
+} from '@angular/fire/auth';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '@portfolio/auth-data-access';
+import { of } from 'rxjs';
 import { AppComponent } from './app.component';
+
+jest.mock('@angular/fire/auth', () => {
+  return {
+    Auth: jest.fn(),
+    signInWithEmailAndPassword: jest.fn(),
+    createUserWithEmailAndPassword: jest.fn(),
+    signOut: jest.fn(),
+    user: jest.fn(() => of(null)),
+  };
+});
 
 describe('AppComponent', () => {
   beforeEach(async () => {
+    (signInWithEmailAndPassword as jest.Mock).mockResolvedValue(
+      {} as UserCredential
+    );
+    (createUserWithEmailAndPassword as jest.Mock).mockResolvedValue(
+      {} as UserCredential
+    );
+    (signOut as jest.Mock).mockResolvedValue(undefined);
+    (user as jest.Mock).mockReturnValue(of(null));
+
     await TestBed.configureTestingModule({
       imports: [AppComponent, RouterModule.forRoot([])],
+      providers: [AuthService, { provide: Auth, useValue: {} }],
     }).compileComponents();
   });
 
